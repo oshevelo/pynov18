bd552590dbb90a89a531b47b4fdd82e2fa043ea9
erge <roman.grabovskiy@hotmail.com>
2022-02-01 21:40:55 +0200

    fix models

--- eshop/catalog/admin.py
+++ eshop/catalog/admin.py
@@ -1,22 +1,52 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from catalog.models import Categories, Products, Manufacturer
-
-
-class CategoriesAdmin(admin.ModelAdmin):
-    pass
+from catalog.models import Category, Product, Manufacturer
 
 
-class ProductsAdmin(admin.ModelAdmin):
-    class Meta:
-        verbose_name = "Product"
-        verbose_name_plural = "Products"
+class CategoryAdmin(admin.ModelAdmin):
+    raw_id_fields = ['parent_cat', ]
+    list_display = ['id', 'get_image', 'parent_cat', 'name', 'slug', 'is_active', ]
+    list_display_links = ['id', "get_image", 'parent_cat', 'name', ]
+    search_fields = ['name', 'description', 'slug', ]
+    list_editable = ['is_active', ]
+    list_filter = ['is_active', 'parent_cat', ]
+    readonly_fields = ['id', 'slug', 'created_at', 'updated_at', 'created_by', 'updated_by']
+    save_on_top = True
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src='{obj.image.url}' width='50'")
+
+    get_image.short_description = "Img"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    raw_id_fields = ['category', 'manufacturer', 'created_by', 'updated_by', ]
+    list_display = ['id', 'get_image', 'name', 'category', 'manufacturer', 'sku',
+                     'price', 'discount', 'quantity', 'attributes', 'is_active',]
+    list_display_links = ['id', "get_image", 'category', 'name', 'manufacturer', 'sku', ]
+    search_fields = ['name', 'description', 'slug', ]
+    readonly_fields = ['id', 'slug', 'created_at', 'updated_at', 'created_by', 'updated_by']
+    list_editable = ['is_active', ]
+
+    save_on_top = True
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src='{obj.image.url}' width='50'")
+
+    get_image.short_description = "Img"
 
 
 class ManufacturerAdmin(admin.ModelAdmin):
-    pass
+    raw_id_fields = ['created_by', 'updated_by', ]
+    list_display = ['id', 'name', 'slug', 'is_active', ]
+    list_editable = ['is_active', ]
+    readonly_fields = ['id', 'slug', 'created_at', 'updated_at', 'created_by', 'updated_by']
+    save_on_top = True
 
 
-admin.site.register(Categories, CategoriesAdmin)
-admin.site.register(Products, ProductsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Manufacturer, ManufacturerAdmin)
--- eshop/catalog/models.py
+++ eshop/catalog/models.py
@@ -4,18 +4,20 @@ from django.db import models
 from django.utils.text import slugify
 
 
-class Categories(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255, blank=True, null=True)
-    slug = models.SlugField(unique=True, editable=False)
-    image = models.ImageField(blank=True)
-    parent_cat = models.ForeignKey("Categories", on_delete=models.PROTECT, blank=True, null=True, )
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True,
+class Category(models.Model):
+    name = models.CharField(verbose_name="category", max_length=255)
+    description = models.TextField(verbose_name="description", max_length=2000, blank=True, null=True)
+    slug = models.SlugField(verbose_name="slug", unique=True, editable=False)
+    image = models.ImageField(verbose_name="image", upload_to="categories/%Y/%m/%d", blank=True)
+    parent_cat = models.ForeignKey("self", verbose_name="parent_category", on_delete=models.PROTECT,
+                                   blank=True,
+                                   null=True, )
+    is_active = models.BooleanField(verbose_name="is_active", default=True)
+    created_at = models.DateTimeField(verbose_name="created_at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="updated_at", auto_now=True)
+    created_by = models.ForeignKey(User, verbose_name="created_by", on_delete=models.PROTECT, null=True, blank=True,
                                    related_name='category_creator')
-    updated_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True,
+    updated_by = models.ForeignKey(User, verbose_name="updated_by", on_delete=models.PROTECT, null=True, blank=True,
                                    related_name='category_updater')
 
     def save(self, *args, **kwargs):
@@ -24,25 +26,26 @@ class Categories(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Category: {self.name}"
+        return self.name
 
     class Meta:
-        db_table = "categories"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(max_length=20000, blank=True, null=True)
     slug = models.SlugField(unique=True, editable=False)
-    image = models.ImageField(blank=True)
-    category = models.ForeignKey("Categories", on_delete=models.PROTECT, blank=True, null=True,
-                                 related_name='product_category')
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, blank=True, null=True,
+                                 related_name='products')
     manufacturer = models.ForeignKey("Manufacturer", on_delete=models.PROTECT, blank=True, null=True,
-                                     related_name='product_manufacturer')
+                                     related_name='products')
     sku = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=5, default=0, decimal_places=2, blank=True, null=True)
-    old_price = models.DecimalField(max_digits=5, default=0, decimal_places=2, blank=True, null=True)
+    discount = models.PositiveIntegerField(default=0, blank=True, null=True)
     quantity = models.PositiveIntegerField(default=0)
     attributes = models.JSONField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -50,7 +53,11 @@ class Products(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True,
                                    related_name='product_creator')
     updated_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True,
-                                   related_name='product_updater')
+                                   related_name='products_updater')
+
+    @property
+    def actual_price(self):
+        return self.price * self.discount if self.discount else self.price
 
     def save(self, *args, **kwargs):
         value = self.name
@@ -58,26 +65,34 @@ class Products(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Product: {self.name}"
+        return self.name
 
     class Meta:
-        db_table = "products"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(max_length=2000, blank=True, null=True)
     slug = models.SlugField(unique=True, editable=False)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(upload_to="manufacturers/%Y/%m/%d", blank=True)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True,
-                                   related_name='manufacturer_creator')
+                                   related_name='creator')
     updated_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True,
-                                   related_name='manufacturer_updater')
+                                   related_name='updater')
+
+    def save(self, *args, **kwargs):
+        value = self.name
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Manufacturer: {self.name}"
+        return self.name
 
     class Meta:
-        db_table = "manufacturer"
+        verbose_name = "Manufacturer"
+        verbose_name_plural = "Manufacturers"
